7d3763da07d94189abaabf36cf7ffbf2f5ff12b4
Paul Kreymborg <paul.kreymborg@softwareag.com>
2018-11-19 13:22:21 -0800

    Renamed container to childContainer in BackgroundWrapper

--- app/scripts/views/pixi_view/backgroundWrapper.ts
+++ app/scripts/views/pixi_view/backgroundWrapper.ts
@@ -5,13 +5,13 @@ import { DragRegistry } from "./dragRegistry.js";
 
 export class BackgroundWrapper {
   public sprite: PIXI.extras.TilingSprite;
-  private container: PIXI.Container;
+  private childContainer: PIXI.Container;
 
   constructor(w: number, h: number, dragRegistry: DragRegistry) {
     var texture = PIXI.Texture.fromImage(BACKGROUND_TILE_PATH);
     this.sprite = new PIXI.extras.TilingSprite(texture, w, h);
-    this.container = new PIXI.Container;
-    this.sprite.addChild(this.container);
+    this.childContainer = new PIXI.Container;
+    this.sprite.addChild(this.childContainer);
 
     this.sprite.interactive = true;
     new BackgroundDragHandler(this, dragRegistry);
@@ -22,15 +22,15 @@ export class BackgroundWrapper {
   }
 
   public addChild(obj: PIXI.DisplayObject): void {
-    this.container.addChild(obj);
+    this.childContainer.addChild(obj);
   }
 
   public addVertex(vtxWrapper: VertexWrapper): void {
-    vtxWrapper.addTo(this.container);
+    vtxWrapper.addTo(this.childContainer);
   }
 
   public removeVertex(vtxWrapper: VertexWrapper): void {
-    vtxWrapper.removeFrom(this.container);
+    vtxWrapper.removeFrom(this.childContainer);
   }
 
   public on(ev: string, fn: Function, context?: any): this {
@@ -48,6 +48,6 @@ export class BackgroundWrapper {
 
   public setPosition(x: number, y: number): void {
     this.sprite.tilePosition.set(x, y);
-    this.container.position.set(x, y);
+    this.childContainer.position.set(x, y);
   }
 }
