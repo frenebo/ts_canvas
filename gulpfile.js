b3972bb9eb6ecfccc778e869d60de7fbd7f32696
Paul Kreymborg <paul.kreymborg@softwareag.com>
2018-12-06 09:28:07 -0800

    Trying to get app working with electron

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,13 @@ gulp.task("clean", () => del(["build"]));
 
 gulp.task("copy", () => gulp.src(["app/**/*", "!app/scripts/**/*.ts"]).pipe(gulp.dest("build")));
 
+gulp.task("copy_deps", () => {
+  return gulp.src(["node_modules/pixi.js/dist/pixi.js"]).pipe(gulp.dest("build/deps/pixi"))
+});
+gulp.task("electron_copy_deps", () => {
+  return gulp.src(["node_modules/pixi.js/dist/pixi.js"]).pipe(gulp.dest("electron_build/deps/pixi"));
+})
+
 gulp.task("start_server", function() {
   return connect.server({
     root: "build",
@@ -25,7 +32,53 @@ gulp.task("ts", function() {
 });
 
 
-gulp.task("serve", gulp.series("clean", "ts", "copy", "start_server"));
+gulp.task("serve", gulp.series(
+  "clean",
+  "ts",
+  "copy",
+  "copy_deps",
+  "start_server",
+));
+
+gulp.task("electron_clean", () => del(["electron_build"]))
+
+gulp.task("electron_ts", function() {
+  return gulp.src(["electron/**/*.ts", "node_modules/types/@types/**/*.d.ts"])
+    .pipe(ts({
+      "module": "commonjs",
+      "moduleResolution": "node",
+      // "types": [
+      //      // add node as an option
+      //      "node"
+      //  ],
+       // "typeRoots" : ["./node_modules/@types"],
+    }))
+    // .pipe(project()).js
+    .pipe(gulp.dest("electron_build"));
+});
+
+gulp.task("electron_copy", function() {
+  return gulp.src(["electron/**/*", "!electron/**/*.ts"]).pipe(gulp.dest("electron_build"))
+});
+
+gulp.task("electron_app_ts", function() {
+  return gulp.src(["app/scripts/**/*.ts", "node_modules/types/@types/**/*.d.ts"])
+    .pipe(project()).js
+    .pipe(gulp.dest("electron_build/app"))
+});
+
+gulp.task("electron_copy_app_images", function() {
+  return gulp.src(["app/images/**/*"]).pipe(gulp.dest("electron_build/images"));
+})
+
+gulp.task("build_electron_app", gulp.series(
+  "electron_clean",
+  "electron_ts",
+  "electron_copy",
+  "electron_app_ts",
+  "electron_copy_app_images",
+  "electron_copy_deps",
+));
 
 gulp.task("tslint", function() {
   var program = tslint.Linter.createProgram("./tsconfig.json");
