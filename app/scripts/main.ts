4eaf51ec843311d6f1bdca226f2add7f1542ca1f
Paul Kreymborg <paulkreymborg@pauls-mbp.sduhsd.lan>
2018-12-12 09:21:33 -0800

    Removed multiple div possibility from main

--- app/scripts/main.ts
+++ app/scripts/main.ts
@@ -2,25 +2,23 @@ import { Messenger } from "./messenger.js";
 import { Model } from "./model/model.js";
 import { View } from "./view/view.js";
 
-export function main(...divs: HTMLDivElement[]): void {
+export function main(div: HTMLDivElement): void {
   const model = new Model();
   const messenger = new Messenger(model);
 
-  for (const div of divs) {
-    const pixiDiv = document.createElement("div");
-    div.appendChild(pixiDiv);
-    const layerDiv = document.createElement("div");
-    div.appendChild(layerDiv);
+  const pixiDiv = document.createElement("div");
+  div.appendChild(pixiDiv);
+  const layerDiv = document.createElement("div");
+  div.appendChild(layerDiv);
 
-    messenger.addView(
-      new View(
-        pixiDiv,
-        messenger.newRequestHandler(),
-        messenger.newInfoRequestHandler(),
-        messenger.newVersioningRequestHandler(),
-      ),
-    ).catch((reason) => {
-      throw new Error(`Could not add view: ${reason}`);
-    });
-  }
+  messenger.addView(
+    new View(
+      pixiDiv,
+      messenger.newRequestHandler(),
+      messenger.newInfoRequestHandler(),
+      messenger.newVersioningRequestHandler(),
+    ),
+  ).catch((reason) => {
+    throw new Error(`Could not add view: ${reason}`);
+  });
 }
